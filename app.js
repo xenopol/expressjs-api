93bd5db6093879895bcd099378f75735bb170c20
Marius Ailisoaie <mariusailisoaie@yahoo.com>
2017-09-26 20:31:44 +0200

    speakers data added

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 import express from "express"
+import data from "./data/data.json"
+
+const PORT = 3000
 
 const app = express()
 
-app.get("/", (req, res)=> res.send("<h1>Salut de la Dani si Marius</h1>"))
+app.get("/", (req, res)=>{
+    let info = ""
+    data.speakers.forEach((speaker)=>{
+        info += `
+        <li>
+            <p>${speaker.name}</p>
+            <p>${speaker.age}</p>
+        </li>
+        `
+    })
+    res.send(`<ul>${info}</ul>`)
+})
 
-const server = app.listen(3000, ()=> console.log("Server running on port 3000"))
+const server = app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`))
